e337d3a3d533720c33a1e486da8b5c1d8e7ca082
sarika <sarikahn29@gmail.com>
2025-11-21 18:54:24 +0530

    final deploy

--- backend/server.js
+++ backend/server.js
@@ -1,10 +1,10 @@
-
 // server.js
 import express from "express";
 import cors from "cors";
 
 const app = express();
 app.use(cors());
+
 app.use(express.json());
 
 app.post("/api/profile", (req, res) => {
